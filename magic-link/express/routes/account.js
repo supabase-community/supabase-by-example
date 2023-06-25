9dd6c28028104210e060af3b3158a393a48291f8
Andrew Smith <a.smith@silentworks.co.uk>
2023-06-26 00:38:11 +0100

    Remove update password functionality from express magic link example (#45)

--- magic-link/express/routes/account.js
+++ magic-link/express/routes/account.js
@@ -3,10 +3,7 @@ var router = express.Router();
 
 const ZodError = require("zod").ZodError;
 const { formatError, success, fault } = require("../lib/utils");
-const {
-  UpdatePasswordSchema,
-  UpdateEmailSchema,
-} = require("../lib/validationSchema");
+const { UpdateEmailSchema } = require("../lib/validationSchema");
 
 const { createClient } = require("../lib/supabase");
 
@@ -54,43 +51,6 @@ router.post("/update-email", async function (req, res, next) {
   res.render(
     "app/account/update-email",
     success("Your email was updated successfully.", { user: session?.user })
-  );
-});
-
-router.get("/update-password", function (req, res, next) {
-  const session = res.locals.session;
-  res.render("app/account/update-password", { user: session?.user });
-});
-router.post("/update-password", async function (req, res, next) {
-  const session = res.locals.session;
-  const { password, passwordConfirm } = req.body;
-  try {
-    UpdatePasswordSchema.parse({ password, passwordConfirm });
-  } catch (err) {
-    if (err instanceof ZodError) {
-      const errs = formatError(err);
-      return res.render("app/account/update-password", {
-        errors: errs,
-        user: session?.user,
-        password,
-      });
-    }
-  }
-
-  const supabase = createClient({ req, res });
-
-  const { error } = await supabase.auth.updateUser({ password });
-
-  if (error) {
-    return res.render(
-      "app/account/update-password",
-      fault("Server error. Try again later.", { password, user: session?.user })
-    );
-  }
-
-  res.render(
-    "app/account/update-password",
-    success("Your password was updated successfully.", { user: session?.user })
   );
 });
 
