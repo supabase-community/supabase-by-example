02bfa1c4bab25f6a1ce7d0fb065f325efc5be88d
Andrew Smith <a.smith@silentworks.co.uk>
2023-08-03 20:59:00 +0000

    Add oauth flow nuxt example

---
+++ oauth-flow/nuxt/app.vue
@@ -0,0 +1,11 @@
+<script setup>
+useHead({
+  title: 'Magic Link + Otp Flow'
+})
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
---
+++ oauth-flow/nuxt/pages/account/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const user = useSupabaseUser()
+</script>
+
+<template>
+    <div class="w-11/12 p-12 px-6 py-10 rounded-lg sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-3/12 sm:px-10 sm:py-6">
+        <h2 class="font-semibold text-4xl mb-4">Account</h2>
+        <p class="font-medium mb-10">
+            Hi {{ user.email }}, this is your account page
+        </p>
+    </div>
+</template>
